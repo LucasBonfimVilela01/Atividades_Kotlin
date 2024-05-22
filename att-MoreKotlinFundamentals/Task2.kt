1a69d4994d875554e36a1b72af5012cc5d2b0f0f
Lucas <125403896+LucasBonfimVilela01@users.noreply.github.com>
2024-05-21 23:25:32 -0300

    Atividade Kotlin MoreKotlinFundamentals

---
+++ att-MoreKotlinFundamentals/Task2.kt
@@ -0,0 +1,13 @@
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING,
+}
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+fun main() {
+}
